e28b9d8f979ba03a0a97f4b6ae6bc9043d04ec15
Nicholas Abate <abaten15@nicks-macbook-pro-4.local>
2019-02-06 09:56:56 -0500

    Initial Commit

---
+++ Billy Baller's Brawlers/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Billy Baller's Brawlers
+//
+//  Created by Nick Abate on 2/6/19.
+//  Copyright © 2019 Nick Abate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Billy Baller's Brawlers/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Billy Baller's Brawlers
+//
+//  Created by Nick Abate on 2/6/19.
+//  Copyright © 2019 Nick Abate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
